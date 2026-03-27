4799563973a4c14887230c81647f852959310db8
DurgeshKumar97 <12duklan.durgesh@gmail.com>
2026-03-27 23:55:18 +0530

    your message

---
+++ resources/views/test.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-red-500 text-3xl">TAILWIND WORKING</h1>
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./app/**/*.php", // 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
