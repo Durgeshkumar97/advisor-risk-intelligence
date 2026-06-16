5568b7ce52adc65e94e7bdae51d2e7bdb362591c
DurgeshKumar97 <12duklan.durgesh@gmail.com>
2026-06-16 13:37:52 +0530

    redirect authenticated users from home to dashboard

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class PageController extends Controller
 {
     public function home()
     {
+        if (auth()->check()) {
+            return redirect()->route('dashboard');
+        }
+
         return view('home');
     }
-} 
+}
